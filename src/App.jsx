bf4594f3000a2b694c0fabe74ce24c7900f2c61d
Thomas Aelbrecht <thomasaelbrecht@live.com>
2021-10-13 11:17:59 +0200

    Add exercise 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import { useCallback, useEffect, useState } from 'react';
+
+function Exercise1() {
+  const [title, setTitle] = useState("");
+
+  useEffect(() => {
+    document.title = title;
+  }, [title]);
+
+  const handleChange = useCallback((e) => setTitle(e.target.value), []);
+
+  return (
+    <>
+      <p>Change the document title:</p>
+      <input type="text" onChange={handleChange} />
+    </>
+  );
+}
+
 function App() {
   return (
     <>
       <h1>Hello CodeSandbox</h1>
+
+      <Exercise1 />
     </>
   );
 }
